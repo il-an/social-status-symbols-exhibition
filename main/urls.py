af0f4a068c382de74d35757382066bc4be644299
ArtScienceMK <2026kulakov.am@student.letovo.ru>
2024-12-17 20:31:42 +0300

    Update home page

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from main.views import index, item_detail
+from main.views import index, item_detail, home_view
 
 
 urlpatterns = [
-    path("", index, name="index"),
+    path('', home_view, name='home'),
     path('item/<int:item_id>/', item_detail, name='item_detail'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
