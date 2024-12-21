b8d0f458af1d0cdb141167f8af07dba4209d6dfd
il-an <150121643+il-an@users.noreply.github.com>
2024-12-21 13:25:41 +0300

    fixes for new deployment

--- exhibition/settings.py
+++ exhibition/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-(i!+27%40c$omw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["exhibition-irving.onrender.com", "127.0.0.1", "il-an-exhibition-socialstudies-d9bd.twc1.net"]
+ALLOWED_HOSTS = ["exhibition-irving.onrender.com", "127.0.0.1", "il-an-exhibition-socialstudies-e06f.twc1.net"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 TAILWIND_APP_NAME = "theme"
 
-CSRF_TRUSTED_ORIGINS = ["https://il-an-exhibition-socialstudies-d9bd.twc1.net"]
+CSRF_TRUSTED_ORIGINS = ["https://il-an-exhibition-socialstudies-e06f.twc1.net"]
 
 STORAGES = {
     "staticfiles": {
